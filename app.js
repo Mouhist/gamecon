eb9abe248d45709e61e4f38a79758e2fafaa71bc
mouhist <mauricioazvedo@gmail.com>
2021-09-19 22:10:00 -0300

    pagina news ja toda posicionada, faltando colorações e efeitos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cadastroRouter = require('./routes/cadastro');
-var cadastroRouter = require('./routes/news');
+var newsRouter = require('./routes/news');
 
 
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cadastro', cadastroRouter);
-app.use('/news', cadastroRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
